b7b86bbb4ddfd0d0f002437b9790bb8bb7722df9
ricardo210 <ricardo21095@gmail.com>
2016-03-28 17:28:59 -0600

    backend connected

--- app/scripts/services/adminService.js
+++ app/scripts/services/adminService.js
@@ -3,6 +3,17 @@ angular.module('RealEstate.Services').factory('adminService', ['$http',
 		$http.defaults.withCredentials = true;
 		var baseUrl = 'http://localhost:8000/';
 		return {
-
+				GetUsers: function(){
+					return $http.get(baseUrl + "v1/users");
+				},
+				PostUsers: function(payload){
+					return $http.post(baseUrl + "v1/register", payload);
+				},
+				UpdateUsers: function(payload){
+					return $http.post(baseUrl + "v1/update", payload);
+				},
+				DeleteUser: function(payload,id){
+					return $http.delete(baseUrl + "v1/erase/"+id, payload);
+				}
 	    };
 }]);
