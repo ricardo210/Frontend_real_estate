a4944b05f44b6c3935790c5c79fae8c1922b691a
ricardo210 <ricardo21095@gmail.com>
2016-03-31 16:52:49 -0600

    tables before merge

--- app/scripts/services/homeService.js
+++ app/scripts/services/homeService.js
@@ -3,6 +3,8 @@ angular.module('RealEstate.Services').factory('homeService', ['$http',
 		$http.defaults.withCredentials = true;
 		var baseUrl = 'http://localhost:8000/';
 		return {
-
+					GetProperty: function(){
+							return $http.get(baseUrl + "v1/properties");
+					}
 	    };
 }]);
