69b38f5b8967e073eecede2464a81e069bacbce4
Nelson Cardenas <nelsonjc.01@gmail.com>
2016-03-31 13:23:16 -0600

    finish frontend contactMe

--- app/scripts/controllers/employeeController.js
+++ app/scripts/controllers/employeeController.js
@@ -1,6 +1,53 @@
 angular.module('RealEstate.Controllers')
   .controller('employeeController', ['$scope', 'employeeService', '$sessionStorage', function ($scope, employeeService, $sessionStorage) {
-    $scope.reloadPage = function () {
-      window.location.reload(true);
-    }
+
+    $scope.title = "Mensajes"
+    $scope.messages = [];
+    $scope.message = {};
+    $scope.temp = {};
+
+        $scope.getMessage = function(){
+          employeeService.GetMessages().then(function(response){
+            $scope.messages = response.data;
+          }).catch(function(err){
+            alert(err.data.error + " " + err.data.message)
+          });
+        }
+
+        $scope.postMessage = function(){
+          $scope.message.state="NO LEÍDO";
+          console.log($scope.message);
+          employeeService.PostMessages($scope.message).then(function(response){
+              alert("Posted to /mail");
+            }).catch(function(err){
+              alert(err.data.error + " " + err.data.message);
+            });
+          }
+
+          $scope.ponerModMensaje =function(object){
+            console.log(object);
+            $scope.mensajeRecibido={
+              id : object._id,
+              state : object.state,
+              name: object.name,
+              email: object.email,
+              number: object.number,
+              message: object.message
+            }
+            $scope.temp=$scope.mensajeRecibido;
+          }
+
+          $scope.readMessage = function(){
+            console.log($scope.temp+ "aqui");
+            employeeService.ReadMessages($scope.temp).then(function(response){
+              alert("message read completed");
+              $scope.getMessages();
+            }).catch(function(err){
+              alert(err.data.error + " " + err.data.message);
+            });
+          }
+
+      $scope.reloadPage = function () {
+        window.location.reload(true);
+      }
   }]);
--- app/scripts/services/employeeService.js
+++ app/scripts/services/employeeService.js
@@ -3,6 +3,17 @@ angular.module('RealEstate.Services').factory('employeeService', ['$http',
 		$http.defaults.withCredentials = true;
 		var baseUrl = 'http://localhost:8000/';
 		return {
+			GetMessages: function(){
+				return $http.get(baseUrl + "v1/load-message");
+			},
 
+			PostMessages: function(payload){
+				return $http.post(baseUrl + "v1/send-message", payload);
+			},
+
+			ReadMessages: function(payload){
+				console.log(payload + "server");
+				return $http.post(baseUrl + "v1/read-message", payload);
+			}
 	    };
 }]);
